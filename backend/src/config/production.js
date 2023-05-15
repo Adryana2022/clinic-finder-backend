8a1c4c079d76ba14c413e5c83c48cefdc19396bf
Matheus José <62911694+matheus00laj@users.noreply.github.com>
2023-05-15 18:06:08 -0300

    att 3

--- backend/src/config/production.js
+++ backend/src/config/production.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
-const port = process.env.PORT?process.env.PORT : 8080
+const port = process.env.PORT?process.env.PORT : 5000
 module.exports ={
     porta:{
         port: port
