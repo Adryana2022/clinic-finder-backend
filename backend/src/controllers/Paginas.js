033a7d758ef8943277acefc51396f459b19dcf2b
Matheus José <62911694+matheus00laj@users.noreply.github.com>
2023-05-15 15:27:25 -0300

    att

--- backend/src/controllers/Paginas.js
+++ backend/src/controllers/Paginas.js
@@ -21,10 +21,10 @@ const cadastrar = async (req, res) => {
             res.status(200)
         }
         else{
-            return res.status(500).json({msg: 'erro ao tentar criar o cadastro: cpf já existente no sistema. entre em contato com o suporte!'})
+            return res.status(500).json('erro ao tentar criar o cadastro: cpf já existente no sistema. entre em contato com o suporte!')
         }
     }else{
-        return res.status(500).json({msg: 'erro ao tentar criar o cadastro: email já existente no sistema. entre em contato com o suporte!'})
+        return res.status(500).json('erro ao tentar criar o cadastro: email já existente no sistema. entre em contato com o suporte!')
     }
     try {
         const senhaCript = await bcrypt.hash(req.body.senha, 10)
@@ -37,7 +37,8 @@ const cadastrar = async (req, res) => {
             bairro: req.body.bairro,
             cidade: req.body.cidade,
             estado: req.body.estado,
-            cep: req.body.cep
+            cep: req.body.cep,
+            numero: req.body.numero
 
         })
         return res.status(200).json({ msg: 'Usuário cadastrado com sucesso!' })
@@ -62,38 +63,61 @@ const verificacaoCadastro =
         body('bairro').notEmpty().withMessage('o campo bairro não pode estar vazio!'),
         body('cidade').notEmpty().withMessage('o campo cidade não pode estar vazio!'),
         body('estado').notEmpty().withMessage('o campo estado não pode estar vazio!'),
-        body('cep').notEmpty().isLength({ min: 8 }).withMessage('o campo cep deve conter 8 caracteres!')
+        body('cep').notEmpty().isLength({ min: 8 }).withMessage('o campo cep deve conter 8 caracteres!'),
+        body('numero')
     ]
 
 const login = async(req, res) => {
-    const user = await userTabela.findOne({where:{email:req.body.email}})
-    // console.log(login.senha)
+    
+    
 
-    if(!user || !bcrypt.compare(req.body.senha, user.senha)){
-        return res.status(401).json({msg:'credenciais inválidas!'})   
+    try{
+        const user = await userTabela.findOne({where:{email:req.body.email}})
+        
+        if(!user || !(bcrypt.compareSync(req.body.senha, user.senha))){
+            res.status(400).json('Login ou senha incorretos!')
+        }else{
+            const token = jwt.sign({userID:user.id}, segredo, {expiresIn:'1h'})
+            res.json({token,msg:'login efetuado com sucesso!'})
+        }
+            
+        
+        
+        
     }
+    catch(err){
+        console.log(err)
+    }
+    // if(!user || !bcrypt.compare(req.body.senha, user.senha)){
+    //     return res.status(401).json({msg:'credenciais inválidas!'})   
+    // }
 
-    const token = jwt.sign({userID:user.id}, segredo, {expiresIn:'1h'})
-    res.json({token})
+    
 
 
 }
 const logado = (req, res)=>{
-
+    res.json('logado!')
 }
-
 function verificarToken(req, res, next){
-    const token = req.headers.authorization?.replace('Bearer','')
+    const token = req.headers.authorization
+    
     if(!token){
-        return res.status(401).json({ error: 'Token não fornecido' });
+        console.log(!token)
+        return res.status(401).json({ error: 'não autorizado!' });
+        
     }
     try{
-        const decodificado = jwt.verify(token, segredo);
-        req.userID = decodificado.userID;
-        next()
+        
+        const decodificado = jwt.verify(token, segredo)
+        req.userID = decodificado.id;
+        
+        res.status(201).json('seja bem-vindo!')
+        return next()
 
     }catch(error){
-        return res.status(401).json({error:error})
+        console.log(error)
+        return res.status(401).json({error})
     }
 }
 
